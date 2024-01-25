eea8cfc464b91d4e038a08450ea034549a539fd2
Ali Tulegenov <220264@astanait.edu.kz>
2024-01-26 05:25:12 +0600

    v1.2

--- server.js
+++ server.js
@@ -2,15 +2,46 @@ const express = require('express');
 const app = express();
 
 app.use(express.static(__dirname + '/public'))
- 
+
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/views/index.html');
-});
-app.get('/travelagency', (req, res) => {
-    res.sendFile(__dirname + '/views/travelagency.html');
+  res.sendFile(__dirname + '/views/index.html');
 });
 
-const port = process.env.PORT || 3000;
+app.get('/:pageName', (req, res) => {
+  const pageName = req.params.pageName;
+
+  switch (pageName) {
+      case 'travelagency':
+          res.sendFile(__dirname + '/views/travelagency.html');
+          break;
+      default:
+          res.status(404).send('Page not found');
+  }
+});
+ 
+
+app.get('/api/cities', async (req, res) => {
+  console.log('Request to /api/cities:', req.query);
+  const country = req.query.country;
+  const cities = await getCitiesForCountry(country);
+  res.send(cities);
+});
+
+async function getCitiesForCountry(country) {
+ 
+  switch (country) {
+      case 'Australia':
+          return ['Sydney', 'Melbourne', 'Brisbane'];
+      case 'Brazil':
+          return ['Sao Paulo', 'Rio de Janeiro', 'Salvador'];
+
+      default:
+          return [];
+  }
+}
+
+
+const port = process.env.PORT || 3001;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
